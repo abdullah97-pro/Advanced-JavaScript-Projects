8b3e338f9adc905f9d7c5aab2ad6368e07f65c00
abdullah97-pro <abdullahahmadi019@gmail.com>
2026-02-22 11:31:38 +0430

    using innerHTML

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+// const container = document.getElementById('container');
+// container.innerHTML = "Hello";
+
+// const container = document.getElementsByClassName('container')[0];
+// container.innerHTML = "Hello Class";
+
+const container = document.querySelector('.container');
+
+let divs = '';
+for (let i = 1; i <= 4; i++) {
+    divs += `<div class="box">Box ${i}</div>`;
+}
+
+container.innerHTML = divs;
