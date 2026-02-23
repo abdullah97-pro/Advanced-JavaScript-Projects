d8db9b4b515f3985fc7ec8276ec9a16371a1b177
abdullah97-pro <abdullahahmadi019@gmail.com>
2026-02-23 11:12:18 +0430

    navbar using js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const nav = document.querySelector('.nav');
+
+let links = [
+    {text: "Home", url: "/home"},
+    {text: "About", url: "/about"},
+    {text: "Contact", url: "/contact"},
+];
+
+for(let i = 0; i < links.length; i++) {
+    let li = document.createElement('li');
+    let a = document.createElement('a');
+
+    li.className = "nav-list";
+    a.className = "nav-item";
+
+    a.textContent = links[i].text;
+    a.href = links[i].url;
+
+    li.appendChild(a);
+    nav.appendChild(li);
+}
